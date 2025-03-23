2362345d9ab4aec6775a2c75cb137e62f7700f1d
Akdev Saha <akdevsaha2003@gmail.com>
2025-03-23 22:46:17 +0530

    detailed hello world

--- basic-01/hello.go
+++ basic-01/hello.go
@@ -1,7 +1,14 @@
+// •	Every Go program starts with a package declaration.
+// •	package main means this is an executable program (Go will look for func main() to start execution).
+// •	If it was package utils, it would be a library (reusable code, no direct execution).
+
 package main
 
 import "fmt"
 
+//	import lets you use code from other packages
+//
+// fmt is a standard Go package for formatting text (printing to console, reading input, etc.).
 func main() {
 	fmt.Println("Hello world, my first go code")
 }
