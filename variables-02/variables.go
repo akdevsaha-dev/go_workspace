1934b00a7a058bf1c5b71035303f4d845f899fdc
Akdev Saha <akdevsaha2003@gmail.com>
2025-03-23 23:07:18 +0530

    variables

---
+++ variables-02/variables.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Akdev Saha"
+	var age int = 21
+
+	day := 0 //shorthand declaration
+	fmt.Println("My name is: ", name, "and my age is: ", age, " and i am on my day", day, "of learing go!")
+}
